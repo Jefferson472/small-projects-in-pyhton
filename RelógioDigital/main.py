09f90684a27edcdaa7e5bf6e98fa891bc9845be5
Jefferson472 <jefferson472@hotmail.com>
2021-10-17 21:03:31 -0300

    Update main.py

--- RelógioDigital/main.py
+++ RelógioDigital/main.py
@@ -2,12 +2,8 @@ from tkinter import *
 from datetime import datetime
 from utils import pickColor
 
-
-# Pyglet simplesmente parou de funcionar, FileNotFoundError: [Errno 2] No such file or directory: 'fonts\\digital-7.ttf'.
-# O comando font = open(font, 'rb') também não funcionou mais.
-# import pyglet
-# pyglet.font.add_file('digital-7.ttf')
-
+import pyglet
+pyglet.font.add_file('digital-7.ttf')
 
 def clockTime():
     time = datetime.now()
@@ -28,46 +24,79 @@ textColor = pickColor.colorGreen
 
 tk = Tk()
 tk.title('Relógio Digital')
-tk.geometry('440x180')
+tk.geometry('440x200')
 tk.resizable(False, False)
 
-# tk.iconbitmap('clock.ico')
+tk.iconbitmap('clock.ico')
 
-def changeColorBg():
-    print('alterou a cor')
-    tk.quit()
+def changeColorBlue():
     bgColor = pickColor.colorBlue    
-    tk.configure(background=bgColor, menu=menuBar)
-    tk.mainloop()
-    
+    textColor = pickColor.colorBlue
+    tk.config(bg=bgColor)
+    label1.config(bg=textColor)
+    label2.config(bg=textColor)
 
 
-def changeColorText():
-    print('alterou a cor')
+def changeColorGreen():
+    bgColor = pickColor.colorGreen    
+    textColor = pickColor.colorGreen
+    tk.config(bg=bgColor)
+    label1.config(bg=textColor)
+    label2.config(bg=textColor)
 
 
-def exitApp():
-    tk.quit()
+def changeColorBlack():
+    bgColor = pickColor.colorBlack
+    textColor = pickColor.colorBlack
+    tk.config(bg=bgColor)
+    label1.config(bg=textColor)
+    label2.config(bg=textColor)
+
+
+def changeColorTextBlue():       
+    textColor = pickColor.colorBlue    
+    label1.config(fg=textColor)
+    label2.config(fg=textColor)
+
+
+def changeColorTextGreen():
+    textColor = pickColor.colorGreen    
+    label1.config(fg=textColor)
+    label2.config(fg=textColor)
+
+
+def changeColorTextBlack():
+    textColor = pickColor.colorBlack   
+    label1.config(fg=textColor)
+    label2.config(fg=textColor)
 
 
 menuBar = Menu(tk)
 optionsMenuBar = Menu(tk, tearoff=0)
-optionsMenuBar.add_command(label='Cor de Fundo', command=changeColorBg)
-optionsMenuBar.add_command(label='Cor da Fonte', command=changeColorText)
-optionsMenuBar.add_separator()
-optionsMenuBar.add_command(label='Sair', command=exitApp)
 menuBar.add_cascade(label='Opçoes', menu=optionsMenuBar)
+
+optionChangeColor = Menu(tk, tearoff=0)
+optionChangeColor.add_command(label='Azul', command=changeColorBlue)
+optionChangeColor.add_command(label='Verde', command=changeColorGreen)
+optionChangeColor.add_command(label='Preto', command=changeColorBlack)
+optionsMenuBar.add_cascade(label='Cor de Fundo', menu=optionChangeColor)
+
+optionChangeTextColor = Menu(tk, tearoff=0)
+optionChangeTextColor.add_command(label='Azul', command=changeColorTextBlue)
+optionChangeTextColor.add_command(label='Verde', command=changeColorTextGreen)
+optionChangeTextColor.add_command(label='Preto', command=changeColorTextBlack)
+optionsMenuBar.add_cascade(label='Cor da Fonte', menu=optionChangeTextColor)
+
+optionsMenuBar.add_separator()
+optionsMenuBar.add_command(label='Sair', command=tk.quit)
 
 tk.configure(background=bgColor, menu=menuBar)
 
-label1 = Label(tk, font=('digital-7 100'), bg=bgColor, fg=textColor)
-label1.grid(row=0, column=0, sticky=N, padx=5)
+label1 = Label(tk, font=('digital-7', 100), bg=bgColor, fg=textColor, width=7)
+label1.grid(row=0, column=0, sticky=N)
 
-label2 = Label(tk, font=('digital-7 20'), bg=bgColor, fg=textColor)
-label2.grid(row=1, column=0, sticky=N, padx=5)
-
-label3 = Label(tk, font=('Arial 5'), bg=bgColor, fg=textColor, text='Por Jefferson Miranda', pady=0)
-label3.grid(row=3, column=0, sticky=E)
+label2 = Label(tk, font=('digital-7', 20), bg=bgColor, fg=textColor)
+label2.grid(row=1, column=0, sticky=N)
 
 
 clockTime()
